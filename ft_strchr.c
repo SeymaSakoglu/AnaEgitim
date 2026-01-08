c659180b79339fdd7a7d8b3dc7690d177ee80d85
Seyma Sakoglu <ssakoglu@k1m21s09.42istanbul.com.tr>
2026-01-08 18:16:19 +0300

    ft_strchr ve ft_strrchr kodlari tamamlandi.

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,22 +6,31 @@
 /*   By: ssakoglu <ssakoglu@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 20:22:57 by ssakoglu          #+#    #+#             */
-/*   Updated: 2026/01/06 20:47:23 by ssakoglu         ###   ########.fr       */
+/*   Updated: 2026/01/08 16:25:45 by ssakoglu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 char	*ft_strchr(const char *str, int c)
 {
 	int	i;
 
 	i = 0;
-	if (str[i] == '\0')
-		return ('\0');
+
 	while (str[i] != '\0')
 	{
-		if (str[i] == c)
-			return (c);
+		if (str[i] == (char)c)
+			return ((char *)(str + i));
 		i++;
 	}
+	if ((char)c == '\0')
+		return ((char *)(str + i));
 	return (NULL);
+}
+
+int	main(void)
+{
+	printf("%s", ft_strchr ("42istanbul", 't'));
+	return (0);
 }
---
+++ ft_strrchr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssakoglu <ssakoglu@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/08 16:26:00 by ssakoglu          #+#    #+#             */
+/*   Updated: 2026/01/08 17:15:59 by ssakoglu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stdio.h>
+
+char	*ft_strrchr(const char *str, int c)
+{
+	int	i;
+	const char	*ptr;
+
+	ptr = NULL;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == (char)c)
+		{
+			ptr = str + i;
+			i++;
+		}
+		else if (str[i] != (char)c)
+			i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)(str + i));
+	return ((char *)ptr);
+}
+
+int	main(void)
+{
+	printf("%s", ft_strrchr ("taktak", 'a'));
+	return (0);
+}
