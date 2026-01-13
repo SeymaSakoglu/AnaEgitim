14d19f5fcdde6a7474c10640d993950d3990c08d
SeymaSakoglu <148337050+SeymaSakoglu@users.noreply.github.com>
2026-01-14 00:21:23 +0300

    strlen , tolower ve to upper dosyalari tamamlandi.

---
+++ ft_strlen.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssakoglu <ssakoglu@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 23:14:55 by ssakoglu          #+#    #+#             */
+/*   Updated: 2026/01/13 23:14:55 by ssakoglu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+size_t	ft_strlen(const char *s)
+{
+	unsigned int	i;
+
+	i = 0;
+	while(*s++)
+		i++;
+	return (i);
+}
+
+int main(void)
+{
+	printf("%zu",ft_strlen("hello"));
+}
