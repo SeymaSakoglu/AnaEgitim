07990dd7899770b7dae4c39a749b0c73666ee8c8
Seyma Sakoglu <ssakoglu@k1m27s03.42istanbul.com.tr>
2026-01-14 12:32:36 +0300

    strnstr ve strncmp dosyalari tamamlandi

---
+++ ft_strncmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssakoglu <ssakoglu@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 18:08:22 by ssakoglu          #+#    #+#             */
+/*   Updated: 2026/01/13 18:42:53 by ssakoglu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned int	i;
+
+	i = 0;
+
+	while (*s1 && *s2 && i < n)
+	{
+		if (s1[i] == s2[i])
+		{
+			i++;
+		}
+		else
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	printf("%d", ft_strncmp("hello", "helL", 5));
+	return (0);
+}
---
+++ ft_strnstr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssakoglu <ssakoglu@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 18:46:18 by ssakoglu          #+#    #+#             */
+/*   Updated: 2026/01/13 19:53:19 by ssakoglu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	if (*needle == '\0')
+		return (*haystack);
+	else
+	{
+		while (haystack[i] != '\0')
+		{
+			i++;
+			if (haystack[i] == needle[j])
+			{
+				while (haystack[i + j] && needle[j] != '\0' && (i + j < len))
+				{
+					j++;
+				}
+			}
+		}
+		return ((char *)haystack[i]);
+	}
+	return (NULL);
+}
+
+int	main(void)
+{
+	printf ("%s", ft_strnstr("merhaba", "hab", 6));
+	return (0);
+}
