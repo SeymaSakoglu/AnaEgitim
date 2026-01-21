551f85bb6d74ae5ce5ed75aa036fe294a2705b95
Seyma Sakoglu <ssakoglu@k1m25s03.42istanbul.com.tr>
2026-01-21 20:55:04 +0300

    ft_put*_fd dosyalari tamamlandi.

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssakoglu <ssakoglu@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 20:33:47 by ssakoglu          #+#    #+#             */
+/*   Updated: 2026/01/21 20:53:45 by ssakoglu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		write(fd, "2147483648", 10);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd('n', fd);
+	}
+	else if (n <= 9)
+		ft_putchar_fd((n + 48), fd);
+	else if (n > 9)
+	{
+		ft_putnbr_fd((n / 10), fd);
+		ft_putnbr_fd((n % 10), fd);
+	}
+}
