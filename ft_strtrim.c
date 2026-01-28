ac2eee78f6db7772d6659b3596ac8fc871427ec7
Seyma Sakoglu <ssakoglu@k1m26s03.42istanbul.com.tr>
2026-01-28 19:27:00 +0300

    strtrim tamamlandi

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ssakoglu <ssakoglu@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 13:09:15 by ssakoglu          #+#    #+#             */
-/*   Updated: 2026/01/26 15:15:22 by ssakoglu         ###   ########.fr       */
+/*   Updated: 2026/01/28 19:22:36 by ssakoglu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,40 @@ char	*ft_strtrim(const char *s1, const char *set)
 	i = 0;
 	j = 0;
 	len = ft_strlen(s1);
-	while (i <= len)
+
+	while (i < len)
 	{
-		while (set[j] != '\0')
+		j = 0;
+		while (set[j])
 		{
-			if (s1[i] != set[j])
-				j++;
-			i++;
-		}
-		break ;
-	}
-	j = 0;
-	while (len > i)
-	{
-		while (set[j] != '\0')
-		{
-			if (s1[len] == set[j])
-				len--;
+			if (s1[i] == set[j])
+			{
+				break ;
+			}
 			j++;
 		}
-		break ;
+		if (set[j] == '\0')
+			break ;
+		i++;
+	}
+	while (len > i)
+	{
+		j = 0;
+		while (set[j])
+		{
+			if (s1[len - 1] == set[j])
+				break ;
+			j++;
+		}
+		if (set[j] == '\0')
+			break ;
+		len--;
 	}
 	copy_s = (unsigned char *)malloc((len - i + 2) * sizeof(char));
 	if (copy_s == NULL)
 		return (NULL);
 	j = 0;
-	while ((i + j) <= len)
+	while ((i + j) < len)
 	{
 		copy_s[j] = s1[i + j];
 		j++;
